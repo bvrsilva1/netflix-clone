074ffc055fa30f63040ace976e06f36cf2699412
Bruno <bvrsilva1@gmail.com>
2020-08-11 18:27:43 -0500

    added trailer popup

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../network/axios'
 import './Row.css'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 const posterBaseUrl = 'https://image.tmdb.org/t/p/original/'
 
 const Row = ({ title, fetchUrl, isOriginalsRow }) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
+
+  const youtubeOptions = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,14 +26,30 @@ const Row = ({ title, fetchUrl, isOriginalsRow }) => {
     fetchData()
   }, [fetchUrl])
 
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          //get url parameters
+          const urlParams = new URLSearchParams(new URL(url).search)
+          //get parameter 'v'
+          setTrailerUrl(urlParams.get('v'))
+        })
+        .catch((error) => console.log('error: ', error))
+    }
+  }
+
   return (
     <div className='row'>
       <h2>{title}</h2>
       <div className='rowPoster'>
         {movies.map((movie) => (
           <img
-            className={`poster ${isOriginalsRow && 'posterOriginals'}`}
             key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`poster ${isOriginalsRow && 'posterOriginals'}`}
             src={`${posterBaseUrl}${
               isOriginalsRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -30,6 +57,7 @@ const Row = ({ title, fetchUrl, isOriginalsRow }) => {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={youtubeOptions} />}
     </div>
   )
 }
